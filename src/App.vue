fa52efebaf9e1caad46255015d5f654d0f72ed34
Mijosé Difuidi <mijose.difuidi@mxns.com>
2024-04-03 17:02:37 +0200

    laravel_jutsu

---
+++ src/App.vue
@@ -0,0 +1,75 @@
+<script setup>
+
+import HelloWorld from '@/components/HelloWorld.vue';
+import { ref, reactive, computed, watch } from 'vue';
+
+// const user = reactive({
+//   name: 'Mijosé',
+//   age: 18
+// });
+
+const name = ref('Mijosé')
+const age = ref(19)
+const sentence = computed(() => {
+    return `Bonjour, je m'appelle ${name.value} et j'ai ${age.value}`
+});
+watch(name, (oldValue, newValue) => {
+  console.log(oldValue, newValue);
+});
+
+const nombre = ref(2);
+
+// const display = () => {
+//   console.log(user.name, user.age);
+// };
+
+const count = () => {
+  nombre.value = nombre.value + 1;
+  console.log(nombre.value);
+}
+
+// display();
+
+</script>
+
+<template>
+
+<!-- <div 
+class="bold"
+
+:class="[ user.age > 20 ? 'text-red' : 'text-green' ]">
+  <HelloWorld>
+    <template v-slot:name> 
+      {{ user.name }}
+    </template>
+    <template v-slot:age>
+      {{ user.age }}
+
+    </template>
+  </HelloWorld>
+</div> -->
+
+  <!-- <input type="text" v-model="user.name" @input="display" ></input>
+<input type="text" v-model="user.age" ></input> -->
+  <button v-on:click="count">Test</button>
+
+  <span> {{ sentence }}</span>
+  <input v-model="name" type="text"></input>
+  <input v-model="age" type="number"></input>
+</template>
+
+<style scoped>
+
+.bold{
+  font-weight: bolder;
+}
+.text-red{
+
+  color: red;
+}
+
+.text-green{
+  color:green;
+}
+</style>
+
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+// const props = defineProps(['user'])
+
+</script>
+
+<template>
+
+<h1> Hello world </h1>
+
+<!-- <p>{{ props.user.name}}, {{ props.user.age}}</p> -->
+<!-- <slot name="name">
+
+</slot>
+
+<slot name="age">
+
+</slot> -->
+  
+</template>
+
